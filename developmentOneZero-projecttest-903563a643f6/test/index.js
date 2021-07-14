76f0af75f0d352df801082575aaf842b44a4f70c
Renan Silva <renansantos.silva04@outlook.com>
2021-07-14 11:51:19 -0300

    Removi o schema local do usuario

--- developmentOneZero-projecttest-903563a643f6/test/index.js
+++ developmentOneZero-projecttest-903563a643f6/test/index.js
@@ -20,25 +20,6 @@ chai.use(chaiJson);
 
 const expect = chai.expect;
 
-//Define o minimo de campos que o usuário deve ter. Geralmente deve ser colocado em um arquivo separado
-const userSchema = {
-    title: "Schema do Usuario, define como é o usuario, linha 24 do teste",
-    type: "object",
-    required: ['nome', 'email', 'idade'],
-    properties: {
-        nome: {
-            type: 'string'
-        },
-        email: {
-            type: 'string'
-        },
-        idade: {
-            type: 'number',
-            minimum: 18
-        }
-    }
-}
-
 //Inicio dos testes
 
 //este teste é simplesmente pra enteder a usar o mocha/chai
