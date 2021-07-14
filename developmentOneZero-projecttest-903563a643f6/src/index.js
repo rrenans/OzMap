1ee9df54480e7f9e4ffd05271d786d99bd202202
Renan Silva <renansantos.silva04@outlook.com>
2021-07-14 11:53:46 -0300

    Arquivo server atualizado para integrar com rotas em outro diretório

--- developmentOneZero-projecttest-903563a643f6/src/index.js
+++ developmentOneZero-projecttest-903563a643f6/src/index.js
@@ -4,25 +4,17 @@
 //mais infos
 //https://github.com/ZijianHe/koa-router
 
-// todas as configuraçoes devem ser passadas via environment variables
-const PORT = process.env.PORT || 3000;
 
 const Koa = require('koa');
-const Router = require('koa-router');
-
 const koa = new Koa();
-var router = new Router();
+const router = require('../router/routes')
+
+// todas as configuraçoes devem ser passadas via environment variables
+const PORT = process.env.PORT || 3000;
 
 //rota simples pra testar se o servidor está online
 router.get('/', async (ctx) => {
   ctx.body = `Seu servidor esta rodando em http://localhost:${PORT}`; //http://localhost:3000/
-});
-
-//Uma rota de exemplo simples aqui.
-//As rotas devem ficar em arquivos separados, /src/controllers/userController.js por exemplo
-router.get('/users', async (ctx) => {
-    ctx.status = 200;
-    ctx.body = {total:0, count: 0, rows:[]}
 });
 
 koa
