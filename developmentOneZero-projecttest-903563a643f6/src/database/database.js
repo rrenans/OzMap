58a4b5ab7c691b11bb26fec97e4bdd586d258954
Renan Silva <renansantos.silva04@outlook.com>
2021-07-15 02:29:40 -0300

    Arquvo que realiza a conexão com o Mongo

--- developmentOneZero-projecttest-903563a643f6/src/database/database.js
+++ developmentOneZero-projecttest-903563a643f6/src/database/database.js
@@ -1,14 +1,52 @@
-const sequelize = require('sequelize')
-const UserModel = require('./userModel')
+// Arquivo que conecta no banco de dados
 
+// Importando mongoose, dependência para Mongo
+const mongoose = require('mongoose')
+
+// Criando conexão e pegando sua url
+const db = mongoose.connection;
+const url = "localhost:27017/ozmap";
+
+// Classe com um construtor que retorna função que realiza a conexão
 class Database {
-  constructor() {
-    this.mysql()
-  }
-  mysql() {
-    this.connection = new sequelize()
-    UserModel.init(this.connection)
-  }
+    constructor() {
+        this.mongo();
+    }
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            `mongodb://${url}`, 
+            {
+                useNewUrlParser: true,
+                useFindAndModify: true,
+                useUnifiedTopology: true
+            }
+        );
+        this.handleConnect();
+    }
+
+    // Função que trata conecão do Mongoose 
+    handleConnect() {
+        db.on('connected', () => {
+            console.log(`Mongoose default connection is open on ${url}`); 
+        });
+
+        db.on('error', err => {
+            console.log(`Mongoose default connection has occured \n${err}`);
+        });
+
+        db.on('disconnected', () => {
+            console.log(`Mongoose default connection is disconnected for ${url}`);
+        });
+
+        process.on('SIGINT', () => {
+            db.close(() => {
+                console.log(
+                    'Mongoose default connection is disconnected due to application termination'
+                );
+                process.exit(0);
+            });
+        });
+    }
 }
 
-module.exports = Database;
+module.exports = new Database;
