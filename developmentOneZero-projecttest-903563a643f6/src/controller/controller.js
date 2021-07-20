7ca715be9c394a1b522c9931c05645e4ff52c67a
Renan Silva <renansantos.silva04@outlook.com>
2021-07-20 00:51:03 -0300

    Implementação básica de funções para os testes

--- developmentOneZero-projecttest-903563a643f6/src/controller/controller.js
+++ developmentOneZero-projecttest-903563a643f6/src/controller/controller.js
@@ -1,12 +1,13 @@
 // Arquivo que contém métodos e rotas
 
 const schema = require('../schema/schema')
+/*
 const bodyParser = require('koa-bodyparser')
 
 const Koa = require('koa');
 const koa = new Koa();
 koa.use(bodyParser())
-
+*/
 const Router = require('koa-router')
 var controller = new Router()
 
@@ -71,10 +72,40 @@ controller
   }
 })
 
-// Resolvendo testes
+// Resolvendo os testes
 .get('/users', async (ctx) => {
   ctx.status = 200;
   ctx.body = {total:0, count: 0, rows:[]}
+})
+.post('/user', async (ctx) => {
+  const nome = 'raupp'
+  const email = 'jose.raupp@devoz.com.br'
+  const idade = 35
+  ctx.status = 201
+  await schema.create({
+    nome,
+    email,
+    idade
+  })
+})
+.get('/user/naoExiste', async (ctx) =>{
+  const userNull = await schema.find({}, "userNull")
+  if (userNull){
+    ctx.status = 404
+  }
+})
+.get('/user/raupp', async (ctx) => {
+  const users = await schema.find({}, "nome email idade")
+  ctx.body = users
+  ctx.status = 200
+})
+.delete('/user/raupp', async (ctx) => {
+  const {nome} = "raupp"
+  await schema.deleteOne({ nome: nome })
+  ctx.status = 200
+})
+.get('/users', async (ctx) => {
+  ctx.body = {total:5}
 })
 
 module.exports = controller;
